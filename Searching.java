eea38503f8125cfabbc7c5f6dba2e9300c938abe
Shubhajit <shubjitgithub@gmail.com>
2024-10-14 15:50:57 +0530

    Done

---
+++ Searching.java
@@ -0,0 +1,18 @@
+// Binary search is the fastest algorithim for searching any element
+
+// int start = your_stating_num;
+// int end = your_ending_num;
+// int n=searching_element;
+// while(start<=end){
+//     int mid=(start+end)/2;
+//     if(n==mid){
+//         return true;
+//     }
+    
+//     else if(n>mid){
+// start=mid+1;
+//     }
+//     else if(n<mid)
+//     end=mid-1;
+
+// }
