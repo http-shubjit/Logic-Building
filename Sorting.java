57f5febfe673599c9405cd4f65b2586cfda0fe35
Shubhajit <shubjitgithub@gmail.com>
2024-10-15 17:26:30 +0530

    Reverse

--- Sorting.java
+++ Sorting.java
@@ -1,9 +1,28 @@
-import java.util.Arrays;
-//Arrays.sort() is fastest method for sorting a array in java
-public class Sorting {
-    public static void main(String[] args) {
-        int[] arr = { 10, 1, 3, 4, 2, 5 };
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-}
+// import java.util.Arrays;
+// Timsort://Arrays.sort() is fastest method for sorting a array in java
+// public class Sorting {
+//     public static void main(String[] args) {
+//         int[] arr = { 10, 1, 3, 4, 2, 5 };
+//         Arrays.sort(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
+
+// import java.util.Arrays;
+
+// class Reverse {
+//     public static void main(String[] args) {
+//         int[] arr = { 10, 1, 3, 4, 2, 5 };
+//         int left = 0;
+//         int right = arr.length-1;
+//         while (left < right) {
+//             int temp = arr[left];
+//             arr[left] = arr[right];
+//             arr[right] = temp;
+//             left++;
+//             right--;
+
+//         }
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
